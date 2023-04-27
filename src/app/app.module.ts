dd247420c9a5b2587d990f47c6059138460a011a
tw1ttt3r <tw1ttt3r@gmail.com>
2023-04-26 21:36:23 -0600

    inital commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WheelComponent } from './wheel/wheel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WheelComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/wheel/wheel.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import BScroll from 'better-scroll';
+import { Wheel } from 'better-scroll';
+
+@Component({
+  selector: 'app-wheel',
+  templateUrl: './wheel.component.html',
+  styleUrls: ['./wheel.component.scss']
+})
+export class WheelComponent implements OnInit {
+
+  public items = [
+    { code: 0, description: "cero" },
+    { code: 1, description: "uno" },
+    { code: 2, description: "dos" },
+    { code: 3, description: "tres" },
+  ]
+  public bs: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    BScroll.use(Wheel)
+  }
+
+  ngAfterViewChecked() {
+    if (this.bs === undefined) {
+      this.bs = new BScroll('.wheel', {
+        wheel: {
+          selectedIndex: 0,
+          wheelWrapperClass: "wheel-container",
+          wheelItemClass: "wheel-item",
+          rotate: 25
+        },
+        probeType: 3
+      })
+    }
+  }
+
+}
